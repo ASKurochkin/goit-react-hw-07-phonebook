ddef3f3597483923462732e48773835c52a026ba
OLEKSANDR KUROCHKIN <kurochkinac@gmail.com>
2023-09-10 08:08:03 +0300

    Create project

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import Container from './Container/Container';
+import { FormPhone } from './Form/FormPhone';
+import { ContactsList } from './ContactsList/ContactsList';
+import Search from './Search/Search';
+import { useSelector } from 'react-redux';
+
+export const App = () => {
+  const { contacts } = useSelector(state => state.contacts);
+
+  return (
+    <>
+      <Container title="Phone book">
+        <FormPhone />
+      </Container>
+      <Container title="Contacts">
+        {contacts.length ? (
+          <>
+            <Search />
+            <ContactsList />
+          </>
+        ) : (
+          <p>Phone book is empty</p>
+        )}
+      </Container>
+    </>
+  );
+};
